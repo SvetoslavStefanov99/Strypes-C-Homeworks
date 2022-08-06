9dbf1ee7dab5d5f17b7942091ebbab0f3a386f08
SvetoslavStefanov99 <87032543+SvetoslavStefanov99@users.noreply.github.com>
2022-08-06 16:28:42 +0300

    Update zad4.c

--- zad4.c
+++ zad4.c
@@ -7,13 +7,11 @@ int main()
     int number3;
     int question;
     printf("Enter three binary numbers:\n");
-    
-    
-        scanf("%d %d %d",&number1, &number2, &number3);
-         if((number1>=0&&number2>=0&&number3>=0)&&(number1<=1&&number2<=1&&number3<=1)){
-             printf("XOR with 2 numbers:%d\n",number1 ^ number2);
-             printf("XOR with 3 numbers:%d\n",number1 ^ number2 ^ number3);
-        }else printf("Enter only binary numbers!\n");
+    scanf("%d %d %d",&number1, &number2, &number3);
+    if((number1>=0&&number2>=0&&number3>=0)&&(number1<=1&&number2<=1&&number3<=1)){
+        printf("XOR with 2 numbers:%d\n",number1 ^ number2);
+        printf("XOR with 3 numbers:%d\n",number1 ^ number2 ^ number3);
+    }else printf("Enter only binary numbers!\n");
     return 0;
 }
     
