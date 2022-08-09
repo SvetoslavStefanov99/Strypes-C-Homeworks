89cd803d214a50a0d102a7ad298d0f7f066766d7
SvetoslavStefanov99 <87032543+SvetoslavStefanov99@users.noreply.github.com>
2022-08-09 16:25:51 +0300

    Update zad4B.c

--- Homework2/zad4B.c
+++ Homework2/zad4B.c
@@ -8,6 +8,7 @@ int main()
     printf("Enter three binary numbers:\n");
     scanf("%d %d %d",&number1, &number2, &number3);
     if((number1>=0&&number2>=0&&number3>=0)&&(number1<=1&&number2<=1&&number3<=1)){
+        //printf("XOR with 3 numbers:%d\n",number1 ^ number2 ^ number3); old formula
         printf("XOR with 3 numbers:%d\n",((!number1 && !number2) && number3) || ((!number1 && number2) && !number3) || ((number1 && !number2) && !number3) || ((number1 && number2) && number3));
     }else printf("Enter only binary numbers!\n");
     return 0;
