b9a7ef45363d1bd0ab8a084dcfba633bb6a9f36d
SvetoslavStefanov99 <87032543+SvetoslavStefanov99@users.noreply.github.com>
2022-08-09 16:23:08 +0300

    Update zad4A.c

--- Homework2/zad4A.c
+++ Homework2/zad4A.c
@@ -7,7 +7,8 @@ int main()
     printf("Enter two binary numbers:\n");
     scanf("%d %d",&number1, &number2);
     if((number1>=0&&number2>=0)&&(number1<=1&&number2<=1)){
-        printf("XOR with 2 numbers:%d\n",(!number1 && number2) || (number1 && !number2));
+        //printf("XOR with 2 numbers:%d\n",number1 ^ number2); old formula
+        printf("XOR with 2 numbers:%d\n",(!number1 && number2) || (number1 && !number2)); //new formula
     }else printf("Enter only binary numbers!\n");
     return 0;
 }
