46bfd02ad7bc87ca4b4dfdb2a88fc9e39cfd1580
SvetoslavStefanov99 <87032543+SvetoslavStefanov99@users.noreply.github.com>
2022-09-02 19:29:14 +0300

    Update zad2.c

--- Homework6&7/zad2.c
+++ Homework6&7/zad2.c
@@ -15,7 +15,7 @@ int main(){
 void Reverse(char* ptr){
     int temp=0;
     unsigned len=strLen(ptr);
-    for(int i=0;i<21/2;i++){
+    for(int i=0;i<len/2;i++){
         temp=ptr[i];
         ptr[i]=ptr[len-i-1];
         ptr[len-i-1]=temp;
