e8f85bbf8a5819efb10997463855745c8499e942
SvetoslavStefanov99 <87032543+SvetoslavStefanov99@users.noreply.github.com>
2022-09-02 19:19:28 +0300

    Add files via upload

---
+++ Homework6&7/zad2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdint.h>
+
+void Reverse(char* ptr);
+unsigned strLen(char* ptr);
+
+int main(){
+    char string[]="Strypes is very nice!";
+    Reverse(string);
+    printf("%s\n",string);
+    return 0;
+
+}
+
+void Reverse(char* ptr){
+    int temp=0;
+    unsigned len=strLen(ptr);
+    for(int i=0;i<21/2;i++){
+        temp=ptr[i];
+        ptr[i]=ptr[len-i-1];
+        ptr[len-i-1]=temp;
+    }
+}
+
+unsigned strLen(char* ptr){
+    unsigned length=0;
+    for(int i=0; ptr[i]!='\0'; i++){
+        length=i;
+    }
+    return length+1;
+}
