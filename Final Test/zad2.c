34b9829fd11818119f10355c396c31726dcbaa5a
SvetoslavStefanov99 <87032543+SvetoslavStefanov99@users.noreply.github.com>
2022-09-10 13:08:11 +0300

    Add files via upload

---
+++ Final Test/zad2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+unsigned sumArrayDigits(const int* arr, size_t n);
+
+int main(){
+    int arr[] = {12, 34, 5, 70, 123};
+    printf("%u\n",sumArrayDigits(arr,5));
+    return 0;
+}
+
+unsigned sumArrayDigits(const int* arr, size_t n){
+    unsigned sum=0;
+    int ones=0;
+    for(int i=0;i<n;i++){
+        int number = arr[i];
+        while (number != 0)
+        {
+            ones = number % 10;
+            number /= 10;
+            sum += ones;
+        }
+    }
+    return sum;
+}
